a78b316f0856776cfa1a9d4973298866d071aa54
hanichoye <hana36593@gmail.com>
2023-06-27 05:05:32 -0700

    Configration

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# To create file in tmp directory
+
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+A Readme file for 0x0A-configuration_management directory
